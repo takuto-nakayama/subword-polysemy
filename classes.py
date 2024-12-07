3942de270412e1b690004e5bca7ab5e7fca6bdff
Takuto Nakayama <tnakayama.a5ling@gmail.com>
2024-12-08 01:26:30 +0900

    update all classes

--- classes.py
+++ classes.py
@@ -4,7 +4,7 @@ from sklearn.decomposition import PCA
 import os, h5py, re, numpy, torch, math, statistics
 
 class Dataset:
-    def __init__(self, path):
+    def __init__(self, path:str):
         # path of a hdf5 file
         self.path = path
         # path is divided into file and directory name
@@ -29,7 +29,7 @@ class Dataset:
                     (f", shape: {obj.shape}, dtype: {obj.dtype}" if isinstance(obj, h5py.Dataset) else "")
             ))
 
-    def keys(self, key='/'):
+    def keys(self, key:str='/'):
         try:
             with h5py.File(self.path, 'r') as h:
                 return list(h[key].keys())
@@ -38,7 +38,7 @@ class Dataset:
         except Exception as e:
             print(f'Error: {e}')            
     
-    def dataset(self, key='/'):
+    def dataset(self, key:str='/'):
         try:
             with h5py.File(self.path, 'r') as h:
                 return numpy.array([i.decode('utf-8') for i in h[key][:]])
@@ -47,7 +47,7 @@ class Dataset:
         except Exception as e:
             print(f'Error: {e}')            
 
-    def write(self, name=str, data=None):
+    def write(self, name:str=None, data:numpy.ndarray=None):
         if name and data:
             with h5py.File(self.path, 'a') as h:
                 h.create_dataset(name=name, data=data)
@@ -60,89 +60,105 @@ class Dataset:
             print('Error: Something is wrong in arguments')
  
 class Embedding:
-    def __init__(self, text=numpy.array, model='bert-base-multilingual-cased', tokenizer='bert-base-multilingual-cased'):
+    def __init__(self, text=numpy.ndarray, model:str='bert-base-multilingual-cased', tokenizer:str='bert-base-multilingual-cased'):
         self.text = text
         self.embeddings = {}
         self.model = BertModel.from_pretrained(model)
         self.tokenizer = BertTokenizer.from_pretrained(tokenizer)
 
-    def embed(self):
-        for txt in self.text:
-            encoded = self.tokenizer(txt.decode('utf-8'), return_tensors='pt', truncation=True, padding=True)
-            subwords = self.tokenizer.convert_ids_to_tokens(encoded['input_ids'][0][1:-1])
-
-            with torch.no_grad():
-                output = self.model(**encoded)
-                embed = output.last_hidden_state.squeeze(0)
-                for sw, emb in zip(subwords, embed):
-                    emb = emb.detach().numpy()
-                    if sw not in self.embeddings:
-                        self.embeddings[sw] = [emb]
-                    else:
-                        self.embeddings[sw].append(emb)
+    def embed(self, gpu:bool=True):
+        if gpu:
+            # CPU -> GPU
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            print(f"Using device: {device}")
+            # txt corresponds to a sentence
+            for txt in self.text:       
+                # get subword tokens
+                encoded = self.tokenizer(txt, return_tensors='pt', truncation=True, padding=True)
+                encoded = {key: value.to(device) for key, value in encoded.items()}
+                subwords = self.tokenizer.convert_ids_to_tokens(encoded['input_ids'][0][1:-1])
+                # get embeddings
+                with torch.no_grad():
+                    output = self.model(**encoded)
+                    embed = output.last_hidden_state.squeeze(0)
+                    for sw, emb in zip(subwords, embed):
+                        emb = emb.cpu().numpy()
+                        if sw not in self.embeddings:
+                            self.embeddings[sw] = [emb]
+                        else:
+                            self.embeddings[sw].append(emb)
+        else:
+            # txt corresponds to a sentence
+            for txt in self.text:
+                # get subword tokens
+                encoded = self.tokenizer(txt, return_tensors='pt', truncation=True, padding=True)
+                subwords = self.tokenizer.convert_ids_to_tokens(encoded['input_ids'][0][1:-1])
+                # get embeddings
+                with torch.no_grad():
+                    output = self.model(**encoded)
+                    embed = output.last_hidden_state.squeeze(0)
+                    for sw, emb in zip(subwords, embed):
+                        emb = emb.detach().numpy()
+                        if sw not in self.embeddings:
+                            self.embeddings[sw] = [emb]
+                        else:
+                            self.embeddings[sw].append(emb)
     
-    def add_embed(self, text=numpy.array):
-        for txt in text:
-            encoded = self.tokenizer(txt.decode('utf-8'), return_tensors='pt', truncation=True, padding=True)
-            subwords = self.tokenizer.convert_ids_to_tokens(encoded['input_ids'][0][1:-1])
-
-            with torch.no_grad():
-                output = self.model(**encoded)
-                embed = output.last_hidden_state.squeeze(0)
-                for sw, emb in zip(subwords, embed):
-                    emb = emb.detach().numpy()
-                    if sw not in self.embeddings:
-                        self.embeddings[sw] = [emb]
-                    else:
-                        self.embeddings[sw].append(emb)
-
-    def savevec(self, hpath=str, name=str):
-        if '/' not in hpath:
-            hfile = hpath
+    def savevec(self, path:str, name:str):
+        # identify the directory and the file
+        if '/' not in path:
+            hfile = path
             hdir = os.listdir(os.getcwd())
         else:
-            match = re.search(r'(.+?\..+?/)(.+)', hpath[::-1])
-            hfile = match.group(1)
-            hdir = os.listdir(match.group(2))
-
+            match = re.search(r'(.+?\..+?/)(.+)', path[::-1])
+            hfile = match.group(1)[:-1][::-1]
+            hdir = os.listdir(match.group(2))[::-1]
+        # save vectors
         if hfile not in hdir:
-            with h5py.File(hpath, 'w') as h:
+            with h5py.File(path, 'w') as h:
                 g = h.create_group(name=name)
                 for sw in self.embeddings:
                     g.create_dataset(name=sw, data=self.embeddings[sw])
         else:
-            with h5py.File(hpath, 'a') as h:
+            with h5py.File(path, 'a') as h:
                 g = h.create_group(name=name)
                 for sw in self.embeddings:
                     g.create_dataset(name=sw, data=self.embeddings[sw])
 
 class Cluster:
-    def __init__(self, embebbing=numpy.array):
+    def __init__(self, embedding=numpy.ndarray):
         self.dbscan = []
+        self.embedding = embedding
 
-    def cluster(self, min=2, pca=False, e=0.5, range=0.5, git=0.1):
+    def cluster(self, min=2, pca=False, e=0.5, dif=0.5, git=0.1):
+        # emb corresponds to a set of embeddings of each subword
         for emb in self.embeddings:
             if len(emb) >= min:
+                # pca version
                 if pca:
                     pca = PCA()
                     emb = pca.fit_transform(emb)
                     index = numpy.where(numpy.cumsum(pca.explained_variance_ratio_) >= 0.9)[0][0] + 1
                     emb = emb[:index]
-
-                dbscan = numpy.full(len(emb), -1)
-                while int(len(emb)*ratio) <= numpy.sum(dbscan==-1):
+                # find the clusters the number of which is the greatest
+                best_dbscan = numpy.full(len(emb), -1)
+                dbscan = DBSCAN(eps=e, min_samples=min, metric='euclidean').fit_predict(emb)
+                while max(dbscan) >= max(best_dbscan):
+                    best_dbscan = dbscan
+                    e += dif
                     dbscan = DBSCAN(eps=e, min_samples=min, metric='euclidean').fit_predict(emb)
-                    e += range
-                self.dbscan.append(dbscan)
+                self.dbscan.append(best_dbscan)
 
     def entropy(self):
         list_entropy = []
-        
+        # dbs corresponds to clusters for each subword
         for dbs in self.dbscan:
             list_num = []
+            # list_num contains the number of how many are in each cluster
             for i in range(0, max(dbs)+1):
                 list_num.append(numpy.sum(dbs==i))
+            # list_entropy contains the entropy of each subword
             for i in list_num:
                 list_entropy.append(-(i / len(dbs)) * math.log(i / len(dbs), 2))
-        
+        # the mean of the entropies is the average entropy of each subword
         return statistics.mean(list_entropy)

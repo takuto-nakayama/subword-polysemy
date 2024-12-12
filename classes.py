dd2e67f82f527c3bcc5c6149129047a205507a85
takuto-nakayama <tnakayama.a5ling@gmail.com>
2024-12-13 07:57:23 +0900

    minor change

--- classes.py
+++ classes.py
@@ -65,7 +65,7 @@ class WikipediaText:
         self.list_title = []
         wikipedia.set_lang(language)
 
-    def load_text(self, language:str, num:int):
+    def random_text(self, language:str, num:int):
         random_title = wikipedia.random()
         page = wikipedia.page(random_title)
         text = page.content
